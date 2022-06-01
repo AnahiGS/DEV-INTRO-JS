7c3b3b25da1e14f7bf00725aa7a5a2602bd076d6
AnahiGS <99105066+AnahiGS@users.noreply.github.com>
2022-05-31 20:03:12 -0500

    Add files via upload

---
+++ Actividad1.js
@@ -0,0 +1,22 @@
+//Ejercicio 1
+var n = prompt("ingresa un número");
+n = n * 2;
+alert(n);
+
+//Ejercicio 2
+var n1=prompt("escribe el primer número");
+var n2=prompt("escribe el segundo número");
+var resultado=(n1-n2)
+alert(resultado);
+
+//Ejercicio 3
+const pi= 3.1416
+const r= 5.3
+const area= pi*(radio**2)
+alert(area) 
+
+//Ejercicio 4
+const D= 8 
+const d= 6 
+const areaRombo= (D*d)/2
+alert(areaRombo)
