a975fcbbdd6f93cfbd96d33eaac0be4af59057e2
AnahiGS <99105066+AnahiGS@users.noreply.github.com>
2022-06-05 19:56:47 -0500

    Add files via upload

---
+++ Actividad5.js
@@ -0,0 +1,26 @@
+//Ejercicio 1
+let num=[1,4,6,10,22,55,46,2,5,0];
+
+for(let i=0;i<=num.length; i++){
+    if(num[i]>3){
+        console.log(num[i]);
+    }
+}
+
+//Ejercicio 2
+let arreglo=[];
+    let i = 0;
+
+while(i<=5){
+    arreglo.length=(i);
+    console.log(arreglo[i]);
+    i++;
+}
+
+//Ejercicio 3
+
+let arreglo=[];
+for(let i=0; i<=5;i++){
+    arreglo.length=(i);
+    console.log(arreglo.length);
+}
